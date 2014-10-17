3e4f554693ff41329fb1c6e2a6f6017e2925628b
penne12 <penne12@allforfunmc.com>
2014-10-17 10:54:26 -0500

    Fake TNT
    Explodes on right click

---
+++ src/main/java/com/allforfunmc/examples/FakeTnt/FakeTNT.java
@@ -0,0 +1,34 @@
+package com.allforfunmc.examples.FakeTnt;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+
+public class FakeTNT extends Block{
+	public FakeTNT(/*NO args*/){
+		super(Material.tnt);
+		this.setBlockName("Fake_TNT");
+		this.setHardness(0F);
+		this.setLightLevel(1F);
+		this.setCreativeTab(CreativeTabs.tabDecorations);
+	}
+	@Override
+	@SideOnly(Side.CLIENT)
+	 public IIcon getIcon(int side, int meta){
+		return Blocks.tnt.getIcon(side, meta);
+	}
+	@Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
+    {
+		world.createExplosion(player, x, y, z, 100F, true);
+		return true;
+    }
+}
